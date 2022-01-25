0a225e3856eb33fa2426c40c5ad64930d3979f17
Luca Bennett <zy18811@bristol.ac.uk>
2022-01-25 20:24:10 +0000

    save/retrieve elo_df from gdrive using api

--- elo_util.py
+++ elo_util.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import pickle
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
+gauth = GoogleAuth()
+scope = ["https://www.googleapis.com/auth/drive"]
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], scope)
+drive = GoogleDrive(gauth)
 
 
 def reset_df(starting_elo=1000):
     elo_df = pd.DataFrame(columns=['Sam','Luca','Amy','Gabriel'], dtype=pd.Float64Dtype)
     elo_df.loc[0] = starting_elo
-    pickle.dump(elo_df, open('elo_df.pkl', 'wb'),protocol=pickle.HIGHEST_PROTOCOL)
+    #pickle.dump(elo_df, open('elo_df.pkl', 'wb'),protocol=pickle.HIGHEST_PROTOCOL)
+    save_elo_df(elo_df)
 
 
 def add_player(name):
     elo_df = pickle.load(open('elo_df.pkl','rb'))
     elo_df[name] = 1000
-    pickle.dump(elo_df, open('elo_df.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+    #pickle.dump(elo_df, open('elo_df.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+    save_elo_df(elo_df)
 
 
 def elo_update(player_a, player_b, score_a, score_b):
@@ -40,14 +50,38 @@ def elo_update(player_a, player_b, score_a, score_b):
         else:
             elo_df[player].iloc[-1] = elo_df[player].values[-2]
 
-    pickle.dump(elo_df, open('elo_df.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+    #pickle.dump(elo_df, open('elo_df.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+    save_elo_df(elo_df)
 
 
 def print_elos():
-    elo_df = pickle.load(open('elo_df.pkl', 'rb'))
+    #elo_df = pickle.load(open('elo_df.pkl', 'rb'))
+    elo_df = get_elo_df()
     pd.options.display.float_format = '{:.0f}'.format
     print(elo_df.iloc[-1].to_string())
 
 
+def save_elo_df(elo_df):
+    pickle.dump(elo_df, open('elo_df.pkl', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+
+    gauth = GoogleAuth()
+    scope = ["https://www.googleapis.com/auth/drive"]
+    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('house-elo-a60bcdce6904.json', scope)
+    drive = GoogleDrive(gauth)
+
+    gfile = drive.CreateFile({'id': f'{os.environ["DRIVE_ID"]}'})
+    gfile.SetContentFile('elo_df.pkl')
+    gfile.Upload()
+
+
+def get_elo_df():
+    file_id = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()[0]['id']
+    download_file = drive.CreateFile({'id': file_id})
+    download_file.GetContentFile('elo_df.pkl')
+    elo_df = pickle.load(open('elo_df.pkl', 'rb'))
+    return elo_df
+
+
 if __name__ == '__main__':
-    reset_df()
+    elo_df = get_elo_df()
+    print(elo_df)
